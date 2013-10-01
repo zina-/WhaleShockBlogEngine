6871771138a9a759696003bd734001a6eef4a7cb
zina <127.0.0.1.zina@gmail.com>
2013-10-01 23:05:00 +0900

    add article model

--- WhaleShockBlogEngine/Models/AccountModel.cs
+++ WhaleShockBlogEngine/Models/AccountModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace WhaleShockBlogEngine.Models
@@ -10,5 +11,19 @@ namespace WhaleShockBlogEngine.Models
     {
         public string Id { get; set; }
         public string Password { get; set; }
+
+        public bool GetIdVaildation()
+        {
+            // 3자리 이상, 12자리 이하
+            // 숫자 및 영문자
+            string idPattern = @"^[a-zA-Z][a-zA-Z0-9]{2,11}$";
+            return Regex.Match(Id, idPattern).Success;
+        }
+        public bool GetPasswordValidation()
+        {
+            // 8자리 이상, 20자리 이하
+            string passwordPattern = @"^[\w\S\d]{8,20}$";
+            return Regex.Match(Password, passwordPattern).Success;
+        }
     }
 }
