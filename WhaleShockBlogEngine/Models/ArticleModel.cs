f39be24444cce9217520b48dca60d9367704c111
zina <127.0.0.1.zina@gmail.com>
2013-10-01 23:06:06 +0900

    add article model

---
+++ WhaleShockBlogEngine/Models/ArticleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhaleShockBlogEngine.Models
+{
+    [Serializable]
+    public class ArticleModel
+    {
+        public string Subject { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public string Writer { get; set; }
+        public string Content { get; set; }
+    }
+}
